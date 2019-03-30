64a853c773616d09972ab90df12376f2f4a398b4
Hasstrup Ezekiel <hasstrup.ezekiel@gmail.com>
2019-03-30 16:39:06 +0100

    builds query

--- app/queries/hubs_query.rb
+++ app/queries/hubs_query.rb
@@ -4,7 +4,7 @@ module Queries
       new.call(arguments)
     end
 
-    def call
+    def call(arguments)
       @search_text = arguments.delete(:search_text)
       @query = arguments.delete(:query)
       @order_by = arguments.delete(:order_by)
@@ -18,8 +18,12 @@ module Queries
 
     private
 
-      def build_query
-        return {} if @filter_by.nil?
-      end
+    def build_query(scope)
+      function_string = @query.delete(:function)
+      return {} if @filter_by.nil?
+      scope = scope.where(@query)
+      return scope if function_string.nil?
+      scope.where('function LIKE ?', "%#{function_string}%")
+    end
   end
 end
